0da5d6379e4eece6a558e2bedb3226bb91353e44
prayash thapa <comppro54@gmail.com>
2020-11-28 13:36:01 +0545

    New commit

--- higher_order_diff_2.py
+++ higher_order_diff_2.py
@@ -1,35 +1,41 @@
-# Higher order differential equation using euler method or RK-2. This is the program that goes up to second order but 
-# it can be scaled up to make it work for higher order 
-import math 
-import matplotlib.pyplot as plt 
+#Solution of higher order differential equation using RK-4 method. This program can is for second order ode. This can be scaled in order to use it for higher
+#order
+import math
+import matplotlib.pyplot as plt
 
 #Function F1 used for m z= dy/dx
 def F1(z):
     return z
 #Function F2 dz/dx=F2
 def F2(x,y,z):
-    return 6*x+3*y-2*z
+    return math.sin(x)+3*z-y
 
 #Initiating data
-x0=0 # Initial values for x,y,z where z=dy/dx
-y0=0
-z0=1
+x0=1 # Initial values for x,y,z where z=dy/dx
+y0=1.2
+z0=0.5
 a=[x0]
 b=[y0]
 c=[z0]
 h=0.2 # Step size 
-#Rk-2 or euler function 
+
+#RK-4 Function 
 
 def y(x0,y0,z0):
     m1=F1(z0)
-    l1=F2(x0,y0,z0)
-    m2=F1(z0+l1*h)
-    l2=F2(x0+h,y0+h*m1,z0+l1*h)
-    m=(m1+m2)/2
-    l=(l1+l2)/2
+    l1= F2(x0,y0,z0)
+    m2=F1(z0+(h/2)*l1)
+    l2=F2(x0+(h/2),y0+(h/2)*m1,z0+(h/2)*l1)
+    m3=F1(z0+(h/2)*l2)
+    l3=F2(x0+h/2,y0+(h/2)*m2,z0+(h/2)*l2)
+    m4=F1(z0+(h)*l3)
+    l4=F2(x0+h,y0+m3*h,z0+l3*h)
+    m=(m1+2*m2+2*m3+m4)/6
+    l=(l1+2*l2+2*l3+l4)/6
     y=y0+m*h
     z=z0+l*h
     return [y,z]
+
 
 for i in range(0,20):
     it = y(x0,y0,z0)
@@ -48,6 +54,21 @@ plt.ylabel('f(x)')
 plt.title('Solution to the differential equation')
 plt.grid('both')# Enable or disable graph
 plt.show()
-   
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
